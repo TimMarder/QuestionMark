6b7e19f4edfbfcfee4add9d3a7ff39cc8c2ddbd1
TimMarder <tmarder@stuy.edu>
2018-03-13 07:25:05 -0400

    added tester

---
+++ QuickSortTester.java
@@ -0,0 +1,22 @@
+public class QuickSortTester{
+
+    //return int array of size s, with each element fr range [0,maxVal)
+    public static int[] buildArray( int s, int maxVal ) {
+	int[] retArr = new int[s];
+	for( int i = 0; i < retArr.length; i++ )
+	    retArr[i] = (int)( maxVal * Math.random() );
+	return retArr;
+    }
+
+    
+    public static void main(String[] args){
+
+	//testing mechanism: uses right index as the pvtPos
+
+	while
+
+
+    }
+
+
+}
