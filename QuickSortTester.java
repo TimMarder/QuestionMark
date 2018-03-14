a3aa0a594415de710ceec40e2541714229503657
bchongstuy <bchong@stuy.edu>
2018-03-13 22:40:36 -0400

    figured out timing

--- QuickSortTester.java
+++ QuickSortTester.java
@@ -1,11 +1,58 @@
+import java.io.*;
+
 public class QuickSortTester{
 
     
     public static void main(String[] args){
+	/***
+   long timeEnd;
+    long timeStart;
+    int ARRAY_SIZE = 1000;
+    int target;
+    int[][] array;
+    System.out.println("Working...");
+    String loadingBar;
+
+    try(FileWriter fw = new FileWriter(filename, true);
+          BufferedWriter bw = new BufferedWriter(fw);
+          PrintWriter out = new PrintWriter(bw)) {
+        out.println( "TRIAL,ARRAY_SIZE,TIME");
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    for(int trial = 0; trial < 100; trial++) {
+      // RUN TESTS 
+      ARRAY_SIZE += 10;
+      array = populate(ARRAY_SIZE);
+      //start timer
+      timeStart = System.currentTimeMillis();
+      //start testing
+      for(int i = 0; i < 100000; i++) {
+        target = (int) (Math.random() * 5000.0);
+        MatrixFinder.locateCoord(array, target);
+      }
+      timeEnd = System.currentTimeMillis();
+
+      // LOG TIME TO CSV
+      try(FileWriter fw = new FileWriter(filename, true);
+          BufferedWriter bw = new BufferedWriter(fw);
+          PrintWriter out = new PrintWriter(bw)) {
+        out.println(trial + "," + ARRAY_SIZE + "," + (timeEnd - timeStart));
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+***/
 
 
+	
 	//testing mechanism: uses right index as the pvtPos on unsorted array
-
+	try(FileWriter fw = new FileWriter("UnsortedRightPVT.csv", true);
+	    BufferedWriter bw = new BufferedWriter(fw);
+	    PrintWriter out = new PrintWriter(bw)) {
+	    out.println( "TRIAL,ARRAY_SIZE,TIME");
+	} catch (IOException e) {
+	    e.printStackTrace();
+	}
 	int sizeURS = 1; //U = unsorted, RS = rightside pvt
 	while(sizeURS<=100){
 	    long time = 0;
@@ -22,11 +69,19 @@ public class QuickSortTester{
 		counter++;	      
 	    }
 	    System.out.println( sizeURS + " ," + (total/1000));
-	    
+
+	    try(FileWriter fw = new FileWriter("UnsortedRightPVT.csv", true);
+		BufferedWriter bw = new BufferedWriter(fw);
+		PrintWriter out = new PrintWriter(bw)) {
+		out.println(sizeURS + "," + total);
+	    } catch (IOException e) {
+		e.printStackTrace();
+	    }
+	     
 	    sizeURS++;
 
 	}
-		//System.out.println("==========BREAK===========");
+	//System.out.println("==========BREAK===========");
 	//////////////////////////////////////////////////////////////////////
 
 	//testing mechanism: uses right index as the pvtPos on sorted ascending array
